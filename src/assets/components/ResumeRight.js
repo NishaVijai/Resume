39cdd6db944148bbfad996e4927d6053f3da6210
Nisha <nishavijai@gmail.com>
2020-03-15 13:31:36 +0100

    UPDATE RESUME COMPONENT DESIGN
    Add updated resumeleft and resumeright component's design

--- src/assets/components/ResumeRight.js
+++ src/assets/components/ResumeRight.js
@@ -21,24 +21,79 @@ export default class ResumeRight extends Component {
 							<p>chingu-voyage 11</p>
 						</div>
 						<hr />
+
 						<div className="project-3 projects">
 							<p>June 2019 - July 2019</p>
 							<p>front-end web developer</p>
-							<p>chingu-voyage 09</p>
+							<p>chingu-voyage 09 - group project</p>
 						</div>
 						<hr />
 
 						<div className="project-4 projects">
-							<p>May 2019 - June 2019</p>
+							<p>May 2019</p>
 							<p>front-end web developer</p>
-							<p>chingu-voyage 08</p>
+							<p>chingu-voyage 09 - prework-solo-project</p>
+						</div>
+						<hr />
+
+						<div className="project-5 projects">
+							<p>March 2019 - April 2019</p>
+							<p>front-end web developer</p>
+							<p>chingu-voyage 08 - group project</p>
+						</div>
+						<hr />
+
+						<div className="project-6 projects">
+							<p>February 2019</p>
+							<p>front-end web developer</p>
+							<p>chingu-voyage 08 - prework-solo-project</p>
 						</div>
 						<hr />
 					</div>
 
 					<div className="education">
 						<h3>education</h3>
-						<p>Self-taught</p>
+						<div className="education-1 degrees">
+							<p>August 2017 - January 2018</p>
+
+							<p>M.Sc. Programme</p>
+						</div>
+
+						<div className="education-2 degrees">
+							<p>August 2016 - January 2017</p>
+
+							<p>Pædagoguddannelsen UCC</p>
+						</div>
+
+						<div className="education-3 degrees">
+							<p>2003 - 2006</p>
+
+							<p>MCA - Bharathidasan University - India</p>
+						</div>
+
+						<div className="education-4 degrees">
+							<p>2000 - 2003</p>
+
+							<p>BCA - Jamal Mohamed College - Bharathidasan University - India</p>
+						</div>
+
+						<div className="education-5 degrees">
+							<p>1998 - 2000</p>
+
+							<p>St.Joseph's Girl's Higher Secondary School - India</p>
+						</div>
+
+						<div className="education-6 degrees">
+							<p>1993 - 1998</p>
+
+							<p>St.Joseph's Girl's Higher Secondary School - India</p>
+						</div>
+
+						<div className="education-6 degrees">
+							<p>1988 - 1993</p>
+
+							<p>Railway School - India</p>
+						</div>
 					</div>
 				</div>
 
@@ -68,47 +123,171 @@ export default class ResumeRight extends Component {
 						</p>
 					</div>
 
+					<hr />
+
 					<div className="project-description">
 						<div className="project-1-description">
-							<p>solo project - built a cloning website</p>
+							<p>
+								Built a cloned Vaersgo website as my second solo project -{' '}
+								<a href=" https://vaersgo.netlify.com/" target="_blank" rel="noopener  noreferrer">
+									Cloned-Værsgo
+								</a>{' '}
+								using React. The final product is deployed using Netlify. Github repository link - {' '}
+								<a
+									href="https://github.com/chingu-voyages/v12-solo-NishaVijai"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V12-Solo-project
+								</a>{' '}
+							</p>
 						</div>
+
 						<div className="project-2-description">
-							<p>solo project - built a cloning website</p>
+							<p>
+								First solo project to build a cloned Kickstarter website -{' '}
+								<a
+									href="https://cloned-kickstarter.netlify.com/"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									Cloned-Kickstarter
+								</a>{' '}
+								using React. Learned to deploy the final product using Netlify. Github repository link - {' '}
+								<a
+									href="https://github.com/chingu-voyages/v11-solo-NishaVijai"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V11-Solo-project
+								</a>{' '}
+							</p>
 						</div>
+
 						<div className="project-3-description">
-							<p>group project - built a cloning website</p>
+							<p>
+								First time experience to build a dynamic web app using REACT with two remote team members. One of the team member chose an app from Dribbble. We have divided the components and built a final MVP together -{' '}
+								<a
+									href="https://chingu-shoe-project.herokuapp.com/"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									Shoe Web App
+								</a>{' '}
+								We used Heroku to deploy our final product. Github repository link -{' '}
+								<a
+									href="https://github.com/chingu-voyages/v9-toucans-team-14"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V09-toucans-team-14
+								</a>{' '}
+							</p>
 						</div>
+
 						<div className="project-4-description">
-							<p>group project - built a cloning website</p>
+							<p>
+							Pre-work solo project experience to build a chrome extension called -{' '}
+								<a
+									href="https://nishavijai.github.io/Chingu-09-prework-project/"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									INITAB
+								</a>{' '}
+								using HTML,CSS and JavaScript. Used GitHub io to deploy the final version. Github repository link -{' '}
+								<a
+									href="https://github.com/NishaVijai/Chingu-09-prework-project"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V09-pre-work Solo Project
+								</a>{' '}
+							</p>
+						</div>
+
+						<div className="project-5-description">
+							<p>
+								First ever group project work to build a static cloned Chingu website using HTML, CSS and JavaScript<a
+									href="https://chingu-voyages.github.io/v8-toucans-team-09/"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									Cloned Chingu Website
+								</a>
+								 with two remote team members from England and Uganda. We used GitHub io to deploy our webpage. Github repository link - {' '}
+								<a
+									href="https://github.com/chingu-voyages/v8-toucans-team-09"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V08-toucans-team-09
+								</a>{' '}
+							</p>
+						</div>
+
+						<div className="project-6-description">
+							<p>
+								Learned to build first solo project <a
+									href="https://nishavijai.github.io/pre-work-solo-project/"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									TickyBot
+								</a>
+								using HTML, CSS and JavaScript. Gained my first experience to deploy a website by GitHub io -{' '}
+								
+								 Github repository link -{' '}
+								<a
+									href="https://github.com/NishaVijai/pre-work-solo-project"
+									target="_blank"
+									rel="noopener  noreferrer"
+								>
+									V08-pre-work Solo Project
+								</a>{' '}
+							</p>
 						</div>
 					</div>
 
-                    <div className="education-description">
-						<div className="project-1-description">
-							<p>solo project - built a cloning website</p>
+					<hr />
+
+					<div className="education-description">
+						<div className="education-1-description">
+							<h4>description</h4>
+							<p>First semester at ITU - M.Sc. programme Software Development, Advanced Computing</p>
 						</div>
-						<div className="project-2-description">
-							<p>solo project - built a cloning website</p>
+
+						<div className="education-2-description">
+							<h4>description</h4>
+							<p>First semester at Pædagoguddannelsen UCC- Hillerød</p>
 						</div>
-						<div className="project-3-description">
-							<p>group project - built a cloning website</p>
+
+						<div className="education-3-description">
+							<h4>description</h4>
+							<p>Masters in Computer Applications</p>
 						</div>
-						<div className="project-4-description">
-							<p>group project - built a cloning website</p>
+
+						<div className="education-4-description">
+							<h4>description</h4>
+							<p>Bachelors in Computer Applications</p>
+						</div>
+
+						<div className="education-5-description">
+							<h4>description</h4>
+							<p>Higher Secondary Schooling</p>
+						</div>
+
+						<div className="education-6-description">
+							<h4>description</h4>
+							<p>Secondary Schooling</p>
+						</div>
+
+						<div className="education-7-description">
+							<h4>description</h4>
+							<p>Primary Schooling</p>
 						</div>
 					</div>
 				</div>
-
-				{/* <div className="first">
-						
-					</div> */}
-
-				{/* <div className="second">
-						
-					</div> */}
-				{/* <div className="third">
-						
-					</div> */}
 			</div>
 		);
 	}
