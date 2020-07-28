3eccf4fd919ed67aec32a71f56032f12fb803775
Nisha <nishavijai@gmail.com>
2020-07-28 14:00:07 +0200

    UPDATE LANGDATA FORMAT

--- src/assets/components/langData.js
+++ src/assets/components/langData.js
@@ -1,13 +1,19 @@
 const langData = [
-    {id: 1,
-    langTitle: "front-end",
-    languages: ["html", "css - Tailwind & Bulma", "react", "javascript"]},
-    {id: 2,
-        langTitle: "back-end",
-        languages: ["python"]},
-    {id: 3,
-        langTitle: "languages",
-        languages: ["english", "danish", "tamil", "hindi", "urdu"]}
+	{
+		id: 1,
+		langTitle: 'front-end',
+		languages: [ 'html', 'css - Tailwind & Bulma', 'react', 'javascript' ]
+	},
+	{
+		id: 2,
+		langTitle: 'back-end',
+		languages: [ 'python' ]
+	},
+	{
+		id: 3,
+		langTitle: 'languages',
+		languages: [ 'english', 'danish', 'tamil', 'hindi', 'urdu' ]
+	}
 ];
 
 export default langData;
