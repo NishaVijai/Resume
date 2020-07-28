75d73e5f568817c4574bdabf61b4118deeab80d4
Nisha <nishavijai@gmail.com>
2020-07-28 14:12:50 +0200

    REMOVE COMMENTED CODES
    Remove codes inside a comment

--- src/assets/components/ResumeRightThird.js
+++ src/assets/components/ResumeRightThird.js
@@ -4,121 +4,15 @@ import educationData from './educationData';
 
 export default class ResumeRightEducation extends Component {
 	render() {
-		const educationComponent = educationData.map((data) => <EducationContainer date={data.date} name={data.name} education={data.education} /> )
+		const educationComponent = educationData.map((data) => (
+			<EducationContainer date={data.date} name={data.name} education={data.education} />
+		));
 		return (
 			<article id="third">
 				<h3>education</h3>
 				<hr className="move-hr" />
-				
-				{/* <div className="hr-container">
-				<hr  />
-				</div> */}
+
 				{educationComponent}
-				
-				{/* <div className="education-container">
-					<div className="education-1 degrees left-section">
-						<p>August 2017 - January 2018</p>
-						<p>M.Sc. Programme</p>
-					</div>
-
-					<div className="education-1-description education-description">
-						<hr className="move-up-hr" />
-						<p className="top-padding">First semester at ITU - M.Sc. programme Software Development, Advanced Computing</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-2 degrees left-section">
-						<p>August 2016 - January 2017</p>
-
-						<p>
-							Pædagoguddannelsen UCC
-						</p>
-					</div>
-
-					<div className="education-2-description education-description">
-						<p>First semester at Pædagoguddannelsen UCC- Hillerød</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-3 degrees left-section">
-						<p>August 2014 - May 2015</p>
-
-						<p>FIF kursus - UCC</p>
-					</div>
-
-					<div className="education-3-description education-description">
-						<p>Forberedelseskursus for indvandrere og flygtninge på UCC- Skovlunde</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-4 degrees left-section">
-						<p>2003 - 2006</p>
-
-						<p>MCA - Bharathidasan University - India</p>
-					</div>
-
-					<div className="education-4-description education-description">
-						<p>Master of Computer Applications</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-5 degrees left-section">
-						<p>2000 - 2003</p>
-
-						<p>
-							BCA - Jamal Mohamed College -
-							Bharathidasan University - India
-						</p>
-					</div>
-
-					<div className="education-5-description education-description">
-						<p>Bachelor of Computer Applications</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-6 degrees left-section">
-						<p>1998 - 2000</p>
-
-						<p>
-							St.Joseph's Girl's Higher Secondary School - India
-						</p>
-					</div>
-
-					<div className="education-6-description education-description">
-						<p>Higher Secondary Schooling</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-7 degrees left-section">
-						<p>1993 - 1998</p>
-
-						<p>
-							St.Joseph's Girl's Higher Secondary School - India
-						</p>
-					</div>
-
-					<div className="education-7-description education-description">
-						<p>Secondary Schooling</p>
-					</div>
-				</div>
-
-				<div className="education-container">
-					<div className="education-8 degrees left-section">
-						<p>1988 - 1993</p>
-
-						<p>Southern Railway School - India</p>
-					</div>
-
-					<div className="education-8-description education-description">
-						<p>Primary Schooling</p>
-					</div>
-				</div> */}
 			</article>
 		);
 	}
