a6eaf967677cd7449224cce6bb17520b166dbc80
Nisha <nishavijai@gmail.com>
2020-07-28 13:24:24 +0200

    REMOVE COMMENTED CODES
    Remove codes inside a comment

--- src/assets/components/ExperienceContainer.js
+++ src/assets/components/ExperienceContainer.js
@@ -1,31 +1,11 @@
 import React from 'react';
 
 export default function ExperienceContainer(props) {
-	// const linkContainer = props.linkOne.map((lin) => {
-	// 	return lin.textFirst !== null && lin.textThird !== null ? (
-	// 		<li>
-	// 			{lin.textFirst}
-	// 			{lin.text}{' '}
-	// 			<a href={lin.url} target="_blank" rel="noopener  noreferrer">
-	// 				{lin.name}
-	// 			</a>{' '}
-	// 			{lin.lastText}
-	// 			{lin.textThird}
-	// 		</li>
-	// 	) : (
-	// 		<li>
-	// 			{lin.text}{' '}
-	// 			<a href={lin.url} target="_blank" rel="noopener  noreferrer">
-	// 				{lin.name}
-	// 			</a>{' '}
-	// 			{lin.lastText}
-	// 		</li>
-	// 	);
-	// });
-	const test = props.links.map((lin) => 
-		<a href={lin.projLink} target="_blank" rel="noopener  noreferrer" title={lin.altText}><img src={lin.projImgLink} alt={lin.altText} height="30px" width="30px" /> {''}
-	</a>
-	)
+	const test = props.links.map((lin) => (
+		<a href={lin.projLink} target="_blank" rel="noopener  noreferrer" title={lin.altText}>
+			<img src={lin.projImgLink} alt={lin.altText} height="30px" width="30px" /> {''}
+		</a>
+	));
 	return (
 		<div className="projects-container">
 			<div className="project-2 projects left-section">
@@ -35,51 +15,10 @@ export default function ExperienceContainer(props) {
 			</div>
 
 			<div className="project-2-description project-description">
-			
 				<ul>
-					{props.description.map((desc) => <li>{desc} </li> )}
+					{props.description.map((desc) => <li>{desc} </li>)}
 					<li>{test}</li>
-					
-					{/* <li>
-					{props.links.map((lin) => <li>{' '}<a href={lin.githubLink} target="_blank" rel="noopener  noreferrer"> link
-						</a>{' '} </li>)}
-					</li> */}
-					
-					{/* <li><a href={lin.url} target="_blank" rel="noopener  noreferrer">
-							{lin.name}
-						</a>{' '}</li> */}
 				</ul>
-				{/* <ul> */}
-				{/* {linkContainer} */}
-				{/* <li>{props.linkOne.map((lin) => <li>{lin.text}{' '}
-						<a href={lin.url} target="_blank" rel="noopener  noreferrer">
-							{lin.name}
-						</a>{' '}{lin.lastText} </li> )} </li> */}
-				{/* {props.linkOne.map((link) => (
-						<li>
-						{link.textOne}{' '}
-						<a href={link.link} target="_blank" rel="noopener  noreferrer">
-							{link.name}
-						</a>{' '}
-						{link.textTwo}
-					</li>))} */}
-
-				{/* <li>
-						{props.githubText}{' '}
-						<a href={props.githubLink} target="_blank" rel="noopener  noreferrer">
-							{props.githubName}
-						</a>{' '}
-						{props.projectLanguages}
-					</li>
-					<li>{props.linkTwo}</li>
-					<li>
-						{props.projectTextOne}{' '}
-						<a href={props.projectLink} target="_blank" rel="noopener  noreferrer">
-							{props.projectName}
-						</a>{' '}
-						{props.projectTextTwo}{' '}
-					</li> */}
-				{/* </ul> */}
 			</div>
 		</div>
 	);
