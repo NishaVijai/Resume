ff5731a7e019310fd600fc24adec0c2ebad5cd95
Nisha <nishavijai@gmail.com>
2020-07-28 14:13:39 +0200

    UPDATE EDUCATIONDATA FORMAT

--- src/assets/components/educationData.js
+++ src/assets/components/educationData.js
@@ -1,36 +1,52 @@
 const educationData = [
-    {id: 1,
-        date: "August 2017 - January 2018",
-    name: "M.Sc. Programme",
-    education: "First semester at ITU - M.Sc. programme Software Development, Advanced Computing"},
-    {id: 2,
-        date: "August 2016 - January 2017",
-    name: "Pædagoguddannelsen UCC",
-    education: "First semester at Pædagoguddannelsen UCC- Hillerød"},
-    {id: 3,
-        date: "August 2014 - May 2015",
-    name: "FIF kursus - UCC",
-    education: "Forberedelseskursus for indvandrere og flygtninge på UCC- Skovlunde"},
-    {id: 4,
-        date: "2003 - 2006",
-    name: "MCA - Bharathidasan University - India",
-    education: "Master of Computer Applications"},
-    {id: 5,
-        date: "2000 - 2003",
-    name: "BCA - Jamal Mohamed College - Bharathidasan University - India",
-    education: "Bachelor of Computer Applications"},
-    {id: 6,
-        date: "1998 - 2000",
-    name: "St.Joseph's Girl's Higher Secondary School - India",
-    education: "Higher Secondary Schooling"},
-    {id: 7,
-        date: "1993 - 1998",
-    name: "St.Joseph's Girl's Higher Secondary School - India",
-    education: "Secondary Schooling"},
-    {id: 8,
-        date: "1988 - 1993",
-    name: "Southern Railway School - India",
-    education: "Primary Schooling"} 
+	{
+		id: 1,
+		date: 'August 2017 - January 2018',
+		name: 'M.Sc. Programme',
+		education: 'First semester at ITU - M.Sc. programme Software Development, Advanced Computing'
+	},
+	{
+		id: 2,
+		date: 'August 2016 - January 2017',
+		name: 'Pædagoguddannelsen UCC',
+		education: 'First semester at Pædagoguddannelsen UCC- Hillerød'
+	},
+	{
+		id: 3,
+		date: 'August 2014 - May 2015',
+		name: 'FIF kursus - UCC',
+		education: 'Forberedelseskursus for indvandrere og flygtninge på UCC- Skovlunde'
+	},
+	{
+		id: 4,
+		date: '2003 - 2006',
+		name: 'MCA - Bharathidasan University - India',
+		education: 'Master of Computer Applications'
+	},
+	{
+		id: 5,
+		date: '2000 - 2003',
+		name: 'BCA - Jamal Mohamed College - Bharathidasan University - India',
+		education: 'Bachelor of Computer Applications'
+	},
+	{
+		id: 6,
+		date: '1998 - 2000',
+		name: "St.Joseph's Girl's Higher Secondary School - India",
+		education: 'Higher Secondary Schooling'
+	},
+	{
+		id: 7,
+		date: '1993 - 1998',
+		name: "St.Joseph's Girl's Higher Secondary School - India",
+		education: 'Secondary Schooling'
+	},
+	{
+		id: 8,
+		date: '1988 - 1993',
+		name: 'Southern Railway School - India',
+		education: 'Primary Schooling'
+	}
 ];
 
 export default educationData;
