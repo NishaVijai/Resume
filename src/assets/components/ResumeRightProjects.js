7ba80fcc4a4b4c138e062352a5c386b3f9b1fcd2
Nisha <nishavijai@gmail.com>
2020-07-28 13:26:41 +0200

    REMOVE COMMENTED CODES
    Remove codes inside a comment

--- src/assets/components/ResumeRightProjects.js
+++ src/assets/components/ResumeRightProjects.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
-import ExperienceContainer from './ExperienceContainer'
-import expData from './experienceData'
+import React, { Component } from 'react';
+import ExperienceContainer from './ExperienceContainer';
+import expData from './experienceData';
 
 export default class ResumeRightProjects extends Component {
-    render() {
-        // const experienceComponent = expData.map((exp) => <ExperienceContainer date={exp.date} title={exp.title} project={exp.project} githubText={exp.githubText} githubLink={exp.githubLink} githubName={exp.githubName} projectLanguages={exp.projectLanguages} projectTextOne={exp.projectTextOne} projectLink={exp.projectLink} projectName={exp.projectName} projectTextTwo={exp.projectTextTwo} linkOne={exp.linkOne} linkTwo={exp.linkTwo} /> )
-
-        // const experienceComponent = expData.map((exp) => <ExperienceContainer date={exp.date} title={exp.title} project={exp.project} linkOne={exp.linkOne} linkTwo={exp.linkTwo} /> )
-
-        const experienceComponent = expData.map((exp) => <ExperienceContainer date={exp.date} title={exp.title} project={exp.project} description={exp.description} links={exp.links} /> )
-        return (
-            <article id="second">
+	render() {
+		const experienceComponent = expData.map((exp) => (
+			<ExperienceContainer
+				date={exp.date}
+				title={exp.title}
+				project={exp.project}
+				description={exp.description}
+				links={exp.links}
+			/>
+		));
+		return (
+			<article id="second">
 				<h2 className="h2-experience">experience</h2>
-                {experienceComponent}
-            </article>
-        )
-    }
+				{experienceComponent}
+			</article>
+		);
+	}
 }
