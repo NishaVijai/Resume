c008fd914c1c697f236a57a6386421a036774142
Nisha <nishavijai@gmail.com>
2020-08-13 20:24:21 +0200

    UPDATE PORTFOLIO LINK

--- src/assets/components/Resume.js
+++ src/assets/components/Resume.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react';
-import ResumeLeft from './ResumeLeft';
-import ResumeRight from './ResumeRight';
+import React, { Component } from "react";
+import ResumeLeft from "./ResumeLeft";
+import ResumeRight from "./ResumeRight";
 
-import './resume.css';
+import "./resume.css";
 
 export default class Resume extends Component {
-	render() {
-		return (
-			<div className="resume-container">
-				<header>
-					<h1 tabIndex="0">front-end web developer</h1>
-					<p id="h1-hr" />
-					<address id="first">
-						<p tabIndex="0">
-							Mail:{' '}
-							<a href="mailto:nishavijai@gmail.com" title="Nisha's g-mail address">
-								nishavijai@gmail.com
-							</a>
-						</p>
+  render() {
+    return (
+      <div className="resume-container">
+        <header>
+          <h1 tabIndex="0">front-end web developer</h1>
+          <p id="h1-hr" />
+          <address id="first">
+            <p tabIndex="0">
+              Mail:{" "}
+              <a
+                href="mailto:nishavijai@gmail.com"
+                title="Nisha's g-mail address"
+              >
+                nishavijai@gmail.com
+              </a>
+            </p>
 
-						<p tabIndex="0">
-							Mobile:{' '}
-							<a href="tel:+45-53265717" title="Nisha's mobile number">
-								+45-53265717
-							</a>
-						</p>
+            <p tabIndex="0">
+              Mobile:{" "}
+              <a href="tel:+45-53265717" title="Nisha's mobile number">
+                +45-53265717
+              </a>
+            </p>
 
-						{/* <p tabIndex="0">
-								Website:{' '}
-								<a
-									href="https://nishasportfolio.netlify.com/"
-									target="_blank"
-									rel="noopener  noreferrer"
-								>
-									nishasportfolio
-								</a>
-							</p> */}
-					</address>
-				</header>
-				<aside>
-					<ResumeLeft />
-				</aside>
+            <p tabIndex="0">
+              Website:{" "}
+              <a
+                href="https://nishas-portfolio-2019.vercel.app/"
+                target="_blank"
+                rel="noopener  noreferrer"
+              >
+                nishasportfolio
+              </a>
+            </p>
+          </address>
+        </header>
+        <aside>
+          <ResumeLeft />
+        </aside>
 
-				<main>
-					<ResumeRight />
-				</main>
-			</div>
-		);
-	}
+        <main>
+          <ResumeRight />
+        </main>
+      </div>
+    );
+  }
 }
