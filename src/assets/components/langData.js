d9938267d201ce109b3abf7651ed82d2f439e194
Nisha <nishavijai@gmail.com>
2020-08-27 11:27:26 +0200

    UPDATE LANGDATA
    ADD NEW SKILLS

--- src/assets/components/langData.js
+++ src/assets/components/langData.js
@@ -1,42 +1,51 @@
 const langData = [
-	{
-		id: 1,
-		langTitle: 'front-end',
-		languages: [
-			{
-				id: 1,
-				lang: 'html'
-			},
-			{
-				id: 2,
-				lang: 'css - Tailwind & Bulma'
-			},
-			{
-				id: 3,
-				lang: 'react'
-			},
-			{
-				id: 4,
-				lang: 'javascript'
-			}
-		]
-	},
-	{
-		id: 2,
-		langTitle: 'back-end',
-		languages: [ { id: 1, lang: 'python' } ]
-	},
-	{
-		id: 3,
-		langTitle: 'languages',
-		languages: [
-			{ id: 1, lang: 'english' },
-			{ id: 2, lang: 'danish' },
-			{ id: 3, lang: 'tamil' },
-			{ id: 4, lang: 'hindi' },
-			{ id: 5, lang: 'urdu' }
-		]
-	}
+  {
+    id: 1,
+    langTitle: "front-end",
+    languages: [
+      {
+        id: 1,
+        lang: "html",
+      },
+      {
+        id: 2,
+        lang: "css - less, Tailwind & Bulma",
+      },
+      {
+        id: 3,
+        lang: "react.js & React storybook",
+      },
+      {
+        id: 4,
+        lang: "javascript",
+      },
+      {
+        id: 5,
+        lang: "typescript",
+      },
+    ],
+  },
+  {
+    id: 2,
+    langTitle: "back-end",
+    languages: [{ id: 1, lang: "python" }],
+  },
+  {
+    id: 3,
+    langTitle: "version-control",
+    languages: [{ id: 1, lang: "git" }],
+  },
+  {
+    id: 4,
+    langTitle: "languages",
+    languages: [
+      { id: 1, lang: "english" },
+      { id: 2, lang: "danish" },
+      { id: 3, lang: "tamil" },
+      { id: 4, lang: "hindi" },
+      { id: 5, lang: "urdu" },
+    ],
+  },
 ];
 
 export default langData;
