b555c1bfd376eeb3f7e24e0d8e5d2ba62bf40120
Nisha <nishavijai@gmail.com>
2020-08-27 17:38:50 +0200

    UPDATE EXPERIENCE CONTAINER AND ITS DESIGN

--- src/assets/components/ExperienceContainer.js
+++ src/assets/components/ExperienceContainer.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React from "react";
 
 export default function ExperienceContainer(props) {
-	const imgList = props.links.map((lin) => (
-		<a key={lin.id} href={lin.projLink} target="_blank" rel="noopener  noreferrer" title={lin.altText}>
-			<img src={lin.projImgLink} alt={lin.altText} loading="lazy" title={lin.imgTitle} />
-		</a>
-	));
+  const imgList = props.links.map((lin) => (
+    <a
+      key={lin.id}
+      href={lin.projLink}
+      target="_blank"
+      rel="noopener  noreferrer"
+      title={lin.altText}
+    >
+      {lin.linkName}{" "}
+      <img
+        src={lin.projImgLink}
+        alt={lin.altText}
+        loading="lazy"
+        title={lin.imgTitle}
+      />
+    </a>
+  ));
 
-	const descriptionContainer = props.description.map((desc) => (
-	<li key={desc.id} tabIndex="0">{desc.des}</li>));
+  const descriptionContainer = props.description.map((desc) => (
+    <li key={desc.id} tabIndex="0">
+      {desc.des}
+    </li>
+  ));
 
-	return (
-		<article className="projects-container">
-			<section className="projects left-section">
-				<p tabIndex="0">{props.date}</p>
-				<p tabIndex="0">{props.title}</p>
-				<p tabIndex="0">{props.project}</p>
-			</section>
+  return (
+    <article className="projects-container">
+      <section className="projects left-section">
+        <p tabIndex="0">{props.date}</p>
+        <p tabIndex="0">{props.title}</p>
+        <p tabIndex="0">{props.project}</p>
+      </section>
 
-			<section className="project-description">
-				<ul>
-					{descriptionContainer}
-					<li tabIndex="0">{imgList}</li>
-				</ul>
-			</section>
-		</article>
-	);
+      <section className="project-description">
+        <ul>
+          {descriptionContainer}
+          <li tabIndex="0">{imgList}</li>
+        </ul>
+      </section>
+    </article>
+  );
 }
