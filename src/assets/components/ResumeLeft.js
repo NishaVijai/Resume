20795cef842c98ed72bed080264d410dcd1a280b
Nisha <nishavijai@gmail.com>
2021-06-30 10:24:37 +0200

    Update LinkedIn badge

--- src/assets/components/ResumeLeft.js
+++ src/assets/components/ResumeLeft.js
@@ -27,6 +27,22 @@ export default class ResumeLeft extends Component {
             proficiency in JavaScript, Typescript, React, Vue, Semantic HTML,
             CSS, and mobile responsive website development.
           </p>
+          <div
+            className="badge-base LI-profile-badge"
+            data-locale="en_US"
+            data-size="medium"
+            data-theme="light"
+            data-type="VERTICAL"
+            data-vanity="noorun-nisha-319976105"
+            data-version="v1"
+          >
+            <a
+              className="badge-base__link LI-simple-link"
+              href="https://dk.linkedin.com/in/noorun-nisha-319976105?trk=profile-badge"
+            >
+              Noorun Nisha
+            </a>
+          </div>
         </article>
 
         <hr />
